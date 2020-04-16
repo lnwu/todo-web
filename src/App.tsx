fe5eaef27d40f3eb7668c730b232308263e2c89f
kevinlnwu <kevinlnwu@tencent.com>
2020-04-16 19:37:52 +0800

    setup add todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { produce } from "immer";
 
-function App() {
+type Todo = {
+  id: string;
+  title: string;
+};
+
+const App = () => {
+  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    const newTodoList = produce(todoList, (draft) => {
+      draft.push({
+        id: Math.random().toString(),
+        title: newTodo,
+      });
+    });
+
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>当前环境: {process.env.REACT_APP_STAGE}</p>
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <button onClick={handleAddTodo}>添加</button>
+        </div>
+        {todoList.length > 0 && (
+          <ul>
+            {todoList.map((todo) => {
+              return <li key={todo.id}>{todo.title}</li>;
+            })}
+          </ul>
+        )}
       </header>
     </div>
   );
-}
+};
 
 export default App;
